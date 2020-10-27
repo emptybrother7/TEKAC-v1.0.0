2a5927d1c561ca526f6471544c4281dc0fa453ea
emptybrother7 <emptybrother7@gmail.com>
2020-10-27 01:00:40 -0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,103 @@
+
+ /* music functions */
+$(function() {
+	$("#music").draggable({ containment: 'window' });
+ });
+
+ $(document).ready(function(){
+	$("#musicClose").click(function(){
+	  $("#music").hide();
+	});
+	$("#musicBtn").click(function(){
+	  $("#music").show();
+	});
+  });
+
+ /* chat functions */
+$(function() {
+	$("#chat").draggable({ containment: 'window' });
+ });
+
+ $(document).ready(function(){
+	$("#chatClose").click(function(){
+	  $("#chat").hide();
+	});
+	$("#chatBtn").click(function(){
+	  $("#chat").show();
+	});
+  });
+
+ /* art functions */
+ $(function() {
+	$("#art").draggable({ containment: 'window' });
+ });
+
+ $(document).ready(function(){
+	$("#artClose").click(function(){
+	  $("#art").hide();
+	});
+	$("#artBtn").click(function(){
+	  $("#art").show();
+	});
+  });
+
+  /* game functions */
+ $(function() {
+	$("#game").draggable({ containment: 'window' });
+ });
+
+ $(document).ready(function(){
+	$("#gameClose").click(function(){
+	  $("#game").hide();
+	});
+	$("#gameBtn").click(function(){
+	  $("#game").show();
+	});
+  });
+
+  /* vhs functions */
+ $(function() {
+	$("#vhs").draggable({ containment: 'window' });
+ });
+
+ $(document).ready(function(){
+	$("#vhsClose").click(function(){
+	  $("#vhs").hide();
+	});
+	$("#vhsBtn").click(function(){
+	  $("#vhs").show();
+	});
+  });
+
+   /* uniswap functions */
+ $(function() {
+	$("#uniswap").draggable({ containment: 'window' });
+ });
+
+ $(document).ready(function(){
+	$("#uniswapClose").click(function(){
+	  $("#uniswap").hide();
+	});
+	$("#uniswapBtn").click(function(){
+	  $("#uniswap").show();
+	});
+  });
+
+
+     /* chart functions */
+ $(function() {
+	$("#chart").draggable({ containment: 'window' });
+ });
+
+ $(document).ready(function(){
+	$("#chartClose").click(function(){
+	  $("#chart").hide();
+	});
+	$("#chartBtn").click(function(){
+	  $("#chart").show();
+	});
+  })
+
+
+
+
